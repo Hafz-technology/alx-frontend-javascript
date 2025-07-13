300d481e0f1745da727915014bd4ea090a478046
Khalil Hafz <129151938+Hafz-technology@users.noreply.github.com>
2025-07-13 13:26:24 +0300

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,18 +1,17 @@
-// Define the Teacher interface
-// It includes mandatory properties and allows for additional, unspecified properties.
+
 interface Teacher {
-  readonly firstName: string; // firstName can only be set during initialization
-  readonly lastName: string;  // lastName can only be set during initialization
-  fullTimeEmployee: boolean; // This attribute must always be defined
-  yearsOfExperience?: number; // This attribute is optional
-  location: string;          // This attribute must always be defined
-  [key: string]: any;        // This index signature allows for any additional properties
-                             // (e.g., 'contract') with any value type.
-                             // Using 'any' here for simplicity as per the example,
-                             // but 'unknown' could be used for stricter type checking.
+  readonly firstName: string; 
+  readonly lastName: string;  
+  fullTimeEmployee: boolean; 
+  yearsOfExperience?: number; 
+  location: string;          
+  [key: string]: any;        
+                            
+                             
+                             
 }
 
-// Example usage of the Teacher interface
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -22,13 +21,13 @@ const teacher3: Teacher = {
 
 
   
-  contract: false, // This is an additional property not explicitly defined in the interface
+  contract: false, 
 };
 
-// Log the teacher3 object to the console
+
 console.log(teacher3);
 
-// You can also create another teacher with yearsOfExperience
+
 const teacher4: Teacher = {
   firstName: 'Jane',
   lastName: 'Smith',
@@ -39,6 +38,5 @@ const teacher4: Teacher = {
 
 console.log(teacher4);
 
-// Attempting to modify readonly properties will result in a TypeScript error:
-// teacher3.firstName = 'Jonathan'; // Error: Cannot assign to 'firstName' because it is a read-only property.
+
 
